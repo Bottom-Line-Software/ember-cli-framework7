926e2e17f854f580587773f5161580314d49e5e3
Izel Nakri <contact@izelnakri.com>
2016-10-28 10:53:16 +0200

    Bottomline npm based build for commit c14d524f72be7c050e8a804c90823d8fb51234e7
    We had a specific commit lockdown for ember-cli-framework7 and our version o the source code has tight references to bower. We moved framework7 reference from bower to npm and intentionally included all the bower and vendor file references here

--- index.js
+++ index.js
@@ -8,22 +8,17 @@ module.exports = {
     this.app = app;
     this._super.included(app);
 
-    //IMPORT CUSTOM EMBER-CLI-FRAMEWORK7 CSS
     app.import('vendor/css/ember-cli-framework7.css');
 
-    var bowerDirectory = app.bowerDirectory;
-    
-    //MATERIAL DESIGN
-    //app.import(bowerDirectory + '/framework7/dist/css/framework7.material.css');
-    //app.import(bowerDirectory + '/framework7/dist/css/framework7.material.colors.css');
+    // MATERIAL DESIGN
+    // app.import('vendor/framework7/dist/css/framework7.material.css');
+    // app.import('vendor/framework7/dist/css/framework7.material.colors.css');
 
-    //IOS DESIGN
-    //app.import(bowerDirectory + '/framework7/dist/css/framework7.ios.css');
-    //app.import(bowerDirectory + '/framework7/dist/css/framework7.ios.colors.css');
+    // IOS DESIGN
+    // app.import('vendor/framework7/dist/css/framework7.ios.css');
+    // app.import('vendor/framework7/dist/css/framework7.ios.colors.css');
 
-    app.import(bowerDirectory + '/framework7/dist/js/framework7.min.js');
-    app.import(bowerDirectory + '/framework7/dist/js/framework7.min.js.map', {
-      destDir: 'assets'
-    });
+    app.import('vendor/framework7/dist/js/framework7.min.js');
+    app.import('vendor/framework7/dist/js/framework7.min.js.map', { destDir: 'assets' });
   }
 };
